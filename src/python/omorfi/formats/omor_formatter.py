b38669dd551202c3ca966179b0b2cc07840f88c4
Frankie Robertson <frankie@robertson.name>
2020-05-14 08:22:27 +0300

    Remove homonym number from WORD_ID

--- src/python/omorfi/formats/omor_formatter.py
+++ src/python/omorfi/formats/omor_formatter.py
@@ -714,12 +714,8 @@ class OmorFormatter(Formatter):
             # do not include normal white space for now
             return ""
         wordmap['stub'] = lexc_escape(wordmap['stub'])
-        if int(wordmap['homonym']) == 1:
-            wordmap['analysis'] = "[WORD_ID=%s]" % (
-                lexc_escape(wordmap['lemma']))
-        else:
-            wordmap['analysis'] = "[WORD_ID=%s_%s]" % (
-                lexc_escape(wordmap['lemma']), wordmap['homonym'])
+        wordmap['analysis'] = "[WORD_ID=%s]" % (
+            lexc_escape(wordmap['lemma']))
         if wordmap['numtype'] and wordmap['numtype'] == 'ORD':
             wordmap['analysis'] += self.stuff2lexc('ADJ')
         else:
